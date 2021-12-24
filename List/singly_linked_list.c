2705419f8163c037cf2deb21f0b5c4d8ca128ca8
surprisedPikachu69 <arvind7447@gmail.com>
2021-12-24 20:53:21 +0530

    first commit

---
+++ List/singly_linked_list.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+void insert_front(struct node *head, int a){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data=a;
+    temp->next=head;
+    head=temp;
+}
+
+void insert_back(struct node *head, int a){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    struct node *current = (struct node*)malloc(sizeof(struct node));
+    temp->data=a;
+    temp->next=NULL;
+    
+    current=head;
+    while(current->next!=NULL){
+        current=current->next;
+    }
+
+    current->next=temp;
+}
+
+void insert_middle(struct node *head, int key, int data)
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp=head;
+
+    while(temp->data!=key && temp!=NULL)
+    {
+        temp=temp->next;
+    }
+
+    if(temp!=NULL){
+        struct node *new_node = (struct node*)malloc(sizeof(struct node));
+        new_node->data=data;
+        new_node->next=temp->next;
+        temp->next=new_node;
+    }
+    else{
+        printf("\nKey not found");
+    }
+}
+
+void display(struct node *head){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp=head;
+    while(temp->next!=NULL){
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    printf("%d->NULL",temp->data);
+}
+int main(){
+    struct node *head = (struct node*)malloc(sizeof(struct node));
+    struct node *second = (struct node*)malloc(sizeof(struct node));
+    struct node *third = (struct node*)malloc(sizeof(struct node));
+    head->data=1;
+    head->next=second;
+
+    second->data=2;
+    second->next=third;
+
+    third->data=4;
+    third->next=NULL;
+    
+    insert_middle(head,2,3);
+    printf("%d",head->next->next->data);
+}
