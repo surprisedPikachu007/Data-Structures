1d0443a3915f99729617ada0af52235185f7fe2e
surprisedPikachu69 <arvind7447@gmail.com>
2022-01-05 18:59:21 +0530

    depth first traversal - revised

--- Graph/depth_first_traversal.c
+++ Graph/depth_first_traversal.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+#define MAX 100
+int stack[MAX];
+int top = -1;
+
+void push(int data)
+{
+    if(top == MAX-1)
+    {
+        printf("Stack Overflow\n");
+        return;
+    }
+
+    top++;
+    stack[top] = data;
+}
+
+void pop()
+{
+    if(top == -1)
+    {
+        printf("Stack Underflow\n");
+        return;
+    }
+
+    printf("%d ",stack[top]);
+    top--;
+    
+}
+
 void dfs(int graph[][5], int n, int v, int visited[]){
     visited[v] = 1;
-    printf("%d ", v);
+    push(v);
     for(int i=0;i<n;i++){
         if(graph[v][i]==1 && visited[i]==0){
             dfs(graph, n, i, visited);
@@ -23,6 +52,13 @@ int main(){
     for(int i=0;i<n;i++){
         visited[i] = 0;
     }
+
     dfs(graph, n, 0, visited);
+    printf("\n");
+
+    for(int i=top;i>=0;i--){
+        pop();
+    }
+
     return 0;
 }
