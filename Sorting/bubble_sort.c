0b3a67ae10c7d0d88012f8db6edc0d79975233d7
surprisedPikachu69 <arvind7447@gmail.com>
2022-03-13 16:57:59 +0530

    bubble sort j to j+1

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -9,9 +9,9 @@ void swap(int *x, int *y)
 
 void bubble_sort(int arr[], int size){
     for(int i=0;i<size-1;i++){
-        for(int j=i;j<size-1;j++){
-            if(arr[i]>arr[j+1]){
-                swap(&arr[i], &arr[j+1]);
+        for(int j=i+1;j<size-1;j++){
+            if(arr[i]>arr[j]){
+                swap(&arr[i], &arr[j]);
             }
         }
     }
