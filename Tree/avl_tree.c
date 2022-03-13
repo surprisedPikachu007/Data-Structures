eee3eaa65536b2afa85c243d5f38ab6f35681e0a
surprisedPikachu69 <arvind7447@gmail.com>
2022-03-13 21:14:04 +0530

    avl tree deletion corrected

--- Tree/avl_tree.c
+++ Tree/avl_tree.c
@@ -108,11 +108,9 @@ struct node* insert(struct node *root, int data){
 }
 
 struct node *delete(struct node *root, int key){
-    struct node *temp;
-
     if(root == NULL){
         return root;
-    }   
+    }
 
     if(key<root->data){
         root->left = delete(root->left, key);
@@ -123,25 +121,49 @@ struct node *delete(struct node *root, int key){
     }
 
     else{
-        if(root->left==NULL || root->right==NULL){
-            temp = root->left? root->left:root->right;
-
-            if(temp==NULL){
-                temp=root;
-                root=NULL;
-            }
-            else{
-                *root = *temp;
-            }
-            free(temp);
+        if(root->left == NULL && root->right == NULL){
+            free(root);
+            return NULL;
         }
 
-        else{
-            temp = findmin(root->right);
-            root->data = temp->data;
-            root->right = delete(root->right, temp->data);
+        if(root->left == NULL){
+            struct node *temp = root->right;
+            free(root);
+            return temp;
         }
+
+        if(root->right == NULL){
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        struct node *temp = findmin(root->right);
+        root->data = temp->data;
+        root->right = delete(root->right, temp->data);
     }
+
+    int balance = balance_factor(root);
+
+    if(balance>1 && balance_factor(root->left)>=0){
+        return right_rotate(root);
+    }
+
+    if(balance>1 && balance_factor(root->left)<0){
+        root->left = left_rotate(root->left);
+        return right_rotate(root);
+    }
+
+    if(balance<-1 && balance_factor(root->right)<=0){
+        return left_rotate(root);
+    }
+
+    if(balance<-1 && balance_factor(root->right)>0){
+        root->right = right_rotate(root->right);
+        return left_rotate(root);
+    }
+
+    return root;
 }
 
 void inorder(struct node *root){
@@ -167,7 +189,7 @@ int main(){
     printf("%d",findmin(root)->data);
     printf("\n");
 
-    delete(root, 20);
+    delete(root, 5);
     inorder(root);
     printf("\n");
 
